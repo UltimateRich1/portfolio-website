548f03fbc2e0ecc6dd8854fca2f4f284de515b74
Richard Lara <r.lara16622@gmail.com>
2023-05-15 21:02:04 -0700

    fist commit - committing all files to the repository

---
+++ script.js
@@ -0,0 +1,12 @@
+const tabLinks = document.getElementsByClassName("tab-links");
+const tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(event, tabName) {
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+}
