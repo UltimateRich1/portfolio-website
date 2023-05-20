d9ee3efeb247a61320a5556011d9999aab39b957
Richard Lara <r.lara16622@gmail.com>
2023-05-19 22:08:07 -0700

    added more stuff

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+// header variables
+const sideMenu = document.getElementById("side-menu");
+
+// about me variables
 const tabLinks = document.getElementsByClassName("tab-links");
 const tabContents = document.getElementsByClassName("tab-contents");
 
+// --------------- header ---------------
+function openMenu() {
+  sideMenu.style.right = "0px";
+}
+
+function closeMenu() {
+  sideMenu.style.right = "-200px";
+}
+
+// --------------- about me ---------------
 function openTab(event, tabName) {
   for (tabLink of tabLinks) {
     tabLink.classList.remove("active-link");
